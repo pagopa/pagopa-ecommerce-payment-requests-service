6a42d085afa5d16eca9ff098b2683fc7b58f5f03
Simone infante <52280205+infantesimone@users.noreply.github.com>
2022-10-25 14:26:17 +0200

    [CHK-707] init setup (#1)
    * feat: init microservice
    
    * feat: add codeowner and pr template

---
+++ src/main/kotlin/it/pagopa/ecommerce/payment/requests/PagopaEcommercePaymentRequestsServiceApplication.kt
@@ -0,0 +1,11 @@
+package it.pagopa.ecommerce.payment.requests
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PagopaEcommercePaymentRequestsServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PagopaEcommercePaymentRequestsServiceApplication>(*args)
+}
