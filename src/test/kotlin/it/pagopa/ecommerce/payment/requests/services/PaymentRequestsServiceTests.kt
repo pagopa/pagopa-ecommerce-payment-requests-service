906ddaa7c53fb927915483fa6c192c47319c5c1b
Fernando-Granato <fernando.granato@pagopa.it>
2023-01-16 15:07:43 +0100

    Feat : first commit for remove the old verify RPT call

--- src/test/kotlin/it/pagopa/ecommerce/payment/requests/services/PaymentRequestsServiceTests.kt
+++ src/test/kotlin/it/pagopa/ecommerce/payment/requests/services/PaymentRequestsServiceTests.kt
@@ -5,7 +5,6 @@ import it.pagopa.ecommerce.generated.payment.requests.server.model.PaymentStatus
 import it.pagopa.ecommerce.generated.payment.requests.server.model.ValidationFaultDto
 import it.pagopa.ecommerce.generated.transactions.model.*
 import it.pagopa.ecommerce.payment.requests.client.NodeForPspClient
-import it.pagopa.ecommerce.payment.requests.client.NodoPerPspClient
 import it.pagopa.ecommerce.payment.requests.domain.RptId
 import it.pagopa.ecommerce.payment.requests.exceptions.InvalidRptException
 import it.pagopa.ecommerce.payment.requests.exceptions.NodoErrorException
@@ -43,13 +42,7 @@ class PaymentRequestsServiceTests {
     private lateinit var paymentRequestsInfoRepository: PaymentRequestInfoRepository
 
     @Mock
-    private lateinit var nodoPerPspClient: NodoPerPspClient
-
-    @Mock
     private lateinit var nodeForPspClient: NodeForPspClient
-
-    @Mock
-    private lateinit var baseNodoVerificaRPTRequest: NodoVerificaRPT
 
     @Mock
     private lateinit var baseVerifyPaymentNoticeReq: VerifyPaymentNoticeReq
@@ -57,20 +50,13 @@ class PaymentRequestsServiceTests {
     @Mock
     private lateinit var nodoOperations: NodoOperations
 
-    @Mock
-    private lateinit var nodoUtilities: NodoUtils
-
     @BeforeEach
     fun init() {
         paymentRequestsService = PaymentRequestsService(
             paymentRequestsInfoRepository,
-            nodoPerPspClient,
             nodeForPspClient,
-            it.pagopa.ecommerce.generated.nodoperpsp.model.ObjectFactory(),
             it.pagopa.ecommerce.generated.transactions.model.ObjectFactory(),
-            baseNodoVerificaRPTRequest,
             baseVerifyPaymentNoticeReq,
-            nodoUtilities,
             nodoOperations
         )
     }
@@ -107,103 +93,12 @@ class PaymentRequestsServiceTests {
     }
 
     @Test
-    fun `should return payment request info from Nodo VerificaRPT`() = runTest {
-        val rptIdAsString = "77777777777302016723749670035"
-        val rptIdAsObject = RptId(rptIdAsString)
-        val description = "Payment request description"
-        val amount = Integer.valueOf(1000)
-        val amountForNodo = BigDecimal.valueOf(amount.toLong())
-        val verificaRPTRisposta = NodoVerificaRPTRisposta()
-        val esitoVerificaRPT = EsitoNodoVerificaRPTRisposta()
-        esitoVerificaRPT.esito = StOutcome.OK.value()
-        val datiPagamento = NodoTipoDatiPagamentoPA()
-        datiPagamento.causaleVersamento = description
-        datiPagamento.importoSingoloVersamento = amountForNodo
-        esitoVerificaRPT.datiPagamentoPA = datiPagamento
-        verificaRPTRisposta.nodoVerificaRPTRisposta = esitoVerificaRPT
-
-        /**
-         * preconditions
-         */
-        given(paymentRequestsInfoRepository.findById(rptIdAsObject)).willReturn(Optional.empty())
-        given(nodoPerPspClient.verificaRpt(any())).willReturn(Mono.just(verificaRPTRisposta))
-        given(nodoOperations.getEuroCentsFromNodoAmount(amountForNodo)).willReturn(amount)
-
-        /**
-         * test
-         */
-        val responseDto = paymentRequestsService.getPaymentRequestInfo(rptIdAsString)
-        /**
-         * assertions
-         */
-        assertEquals(rptIdAsString, responseDto.rptId)
-        assertEquals(description, responseDto.description)
-        assertNull(responseDto.dueDate)
-        assertEquals(amount, responseDto.amount)
-    }
-
-    @Test
-    fun `should return payment request info from Nodo VerificaRPTWithEnteBeneficiario`() = runTest {
-        val rptIdAsString = "77777777777302016723749670035"
-        val rptIdAsObject = RptId(rptIdAsString)
-        val paTaxCode = "77777777777"
-        val paName = "Pa Name"
-        val description = "Payment request description"
-        val amount = Integer.valueOf(1000)
-        val amountForNodo = BigDecimal.valueOf(amount.toLong())
-
-        val verificaRPTRisposta = NodoVerificaRPTRisposta()
-        val esitoVerificaRPT = EsitoNodoVerificaRPTRisposta()
-        esitoVerificaRPT.esito = StOutcome.OK.value()
-        val datiPagamento = NodoTipoDatiPagamentoPA()
-        datiPagamento.causaleVersamento = description
-        datiPagamento.importoSingoloVersamento = amountForNodo
-        val ente = CtEnteBeneficiario()
-        ente.denominazioneBeneficiario = paName
-        val paId = CtIdentificativoUnivocoPersonaG()
-        paId.codiceIdentificativoUnivoco = paTaxCode
-        ente.identificativoUnivocoBeneficiario = paId
-        datiPagamento.enteBeneficiario = ente
-        esitoVerificaRPT.datiPagamentoPA = datiPagamento
-        verificaRPTRisposta.nodoVerificaRPTRisposta = esitoVerificaRPT
-
-        /**
-         * preconditions
-         */
-        given(paymentRequestsInfoRepository.findById(rptIdAsObject)).willReturn(Optional.empty())
-        given(nodoPerPspClient.verificaRpt(any())).willReturn(Mono.just(verificaRPTRisposta))
-        given(nodoOperations.getEuroCentsFromNodoAmount(amountForNodo)).willReturn(amount)
-
-        /**
-         * test
-         */
-        val responseDto = paymentRequestsService.getPaymentRequestInfo(rptIdAsString)
-        /**
-         * assertions
-         */
-        assertEquals(rptIdAsString, responseDto.rptId)
-        assertEquals(description, responseDto.description)
-        assertNull(responseDto.dueDate)
-        assertEquals(amount, responseDto.amount)
-        assertEquals(paName, responseDto.paName)
-        assertEquals(paTaxCode, responseDto.paFiscalCode)
-    }
-
-    @Test
     fun `should return payment request info from Nodo VerifyPaymentNotice`() = runTest {
         val rptIdAsString = "77777777777302016723749670035"
         val rptIdAsObject = RptId(rptIdAsString)
         val description = "Payment request description"
         val amount = Integer.valueOf(1000)
         val amountForNodo = BigDecimal.valueOf(amount.toLong())
-
-        val verificaRPTRisposta = NodoVerificaRPTRisposta()
-        val esitoVerificaRPT = EsitoNodoVerificaRPTRisposta()
-        esitoVerificaRPT.esito = StOutcome.KO.value()
-        val fault = FaultBean()
-        fault.faultCode = "PPT_MULTI_BENEFICIARIO"
-        esitoVerificaRPT.fault = fault
-        verificaRPTRisposta.nodoVerificaRPTRisposta = esitoVerificaRPT
 
         val verifyPaymentNotice = VerifyPaymentNoticeRes()
         verifyPaymentNotice.outcome = StOutcome.OK
@@ -218,7 +113,6 @@ class PaymentRequestsServiceTests {
          * preconditions
          */
         given(paymentRequestsInfoRepository.findById(rptIdAsObject)).willReturn(Optional.empty())
-        given(nodoPerPspClient.verificaRpt(any())).willReturn(Mono.just(verificaRPTRisposta))
         given(nodeForPspClient.verifyPaymentNotice(any())).willReturn(Mono.just(verifyPaymentNotice))
         given(nodoOperations.getEuroCentsFromNodoAmount(amountForNodo)).willReturn(amount)
 
@@ -236,58 +130,6 @@ class PaymentRequestsServiceTests {
     }
 
     @Test
-    fun `should return payment ongoing from verifica RPT`() = runTest {
-        val rptIdAsString = "77777777777302016723749670035"
-        val rptIdAsObject = RptId(rptIdAsString)
-        val verificaRPTRisposta = NodoVerificaRPTRisposta()
-        val esitoVerificaRPT = EsitoNodoVerificaRPTRisposta()
-        esitoVerificaRPT.esito = StOutcome.KO.value()
-        val fault = FaultBean()
-        fault.faultCode = PaymentStatusFaultDto.PPT_PAGAMENTO_IN_CORSO.value
-        esitoVerificaRPT.fault = fault
-        verificaRPTRisposta.nodoVerificaRPTRisposta = esitoVerificaRPT
-
-        /**
-         * preconditions
-         */
-        given(paymentRequestsInfoRepository.findById(rptIdAsObject)).willReturn(Optional.empty())
-        given(nodoUtilities.getCodiceIdRpt(any())).willReturn(NodoTipoCodiceIdRPT())
-        given(nodoPerPspClient.verificaRpt(any())).willReturn(Mono.just(verificaRPTRisposta))
-        /**
-         * assertions
-         */
-        assertThrows<NodoErrorException> {
-            paymentRequestsService.getPaymentRequestInfo(rptIdAsString)
-        }
-    }
-
-    @Test
-    fun `should return payment unknown from verifica RPT`() = runTest {
-        val rptIdAsString = "77777777777302016723749670035"
-        val rptIdAsObject = RptId(rptIdAsString)
-        val verificaRPTRisposta = NodoVerificaRPTRisposta()
-        val esitoVerificaRPT = EsitoNodoVerificaRPTRisposta()
-        esitoVerificaRPT.esito = StOutcome.KO.value()
-        val fault = FaultBean()
-        fault.faultCode = ValidationFaultDto.PPT_DOMINIO_SCONOSCIUTO.value
-        esitoVerificaRPT.fault = fault
-        verificaRPTRisposta.nodoVerificaRPTRisposta = esitoVerificaRPT
-
-        /**
-         * preconditions
-         */
-        given(paymentRequestsInfoRepository.findById(rptIdAsObject)).willReturn(Optional.empty())
-        given(nodoUtilities.getCodiceIdRpt(any())).willReturn(NodoTipoCodiceIdRPT())
-        given(nodoPerPspClient.verificaRpt(any())).willReturn(Mono.just(verificaRPTRisposta))
-        /**
-         * assertions
-         */
-        assertThrows<NodoErrorException> {
-            paymentRequestsService.getPaymentRequestInfo(rptIdAsString)
-        }
-    }
-
-    @Test
     fun `should return payment info request from nodo with VerifyPaymentNotice with due date`() = runTest {
         val rptIdAsString = "77777777777302016723749670035"
         val rptIdAsObject = RptId(rptIdAsString)
@@ -298,14 +140,6 @@ class PaymentRequestsServiceTests {
         val format: DateFormat = SimpleDateFormat("yyyy-MM-dd")
         val date = format.parse("2022-04-24")
         val dueDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(format.format(date))
-
-        val verificaRPTRIsposta = NodoVerificaRPTRisposta()
-        val esitoVerificaRPT = EsitoNodoVerificaRPTRisposta()
-        esitoVerificaRPT.esito = StOutcome.KO.value()
-        val fault = FaultBean()
-        fault.faultCode = "PPT_MULTI_BENEFICIARIO"
-        esitoVerificaRPT.fault = fault
-        verificaRPTRIsposta.nodoVerificaRPTRisposta = esitoVerificaRPT
 
         val verifyPaymentNotice = VerifyPaymentNoticeRes()
         verifyPaymentNotice.outcome = StOutcome.OK
@@ -321,7 +155,6 @@ class PaymentRequestsServiceTests {
          * preconditions
          */
         given(paymentRequestsInfoRepository.findById(rptIdAsObject)).willReturn(Optional.empty())
-        given(nodoPerPspClient.verificaRpt(any())).willReturn(Mono.just(verificaRPTRIsposta))
         given(nodeForPspClient.verifyPaymentNotice(any())).willReturn(Mono.just(verifyPaymentNotice))
         given(nodoOperations.getEuroCentsFromNodoAmount(amountForNodo)).willReturn(amount)
 
@@ -339,93 +172,6 @@ class PaymentRequestsServiceTests {
     }
 
     @Test
-    fun `should get fault from code`() = runTest {
-        val rptIdAsString = "77777777777302016723749670035"
-        val rptIdAsObject = RptId(rptIdAsString)
-
-        val verificaRPTRIsposta = NodoVerificaRPTRisposta()
-        val esitoVerificaRPT = EsitoNodoVerificaRPTRisposta()
-        esitoVerificaRPT.esito = StOutcome.KO.value()
-        val fault = FaultBean()
-        fault.faultCode = "PPT_ERRORE_EMESSO_DA_PAA"
-
-        esitoVerificaRPT.fault = fault
-        verificaRPTRIsposta.nodoVerificaRPTRisposta = esitoVerificaRPT
-
-
-        /**
-         * preconditions
-         */
-        given(paymentRequestsInfoRepository.findById(rptIdAsObject)).willReturn(Optional.empty())
-        given(nodoPerPspClient.verificaRpt(any())).willReturn(Mono.just(verificaRPTRIsposta))
-
-        val exception = assertThrows<NodoErrorException> {
-            paymentRequestsService.getPaymentRequestInfo(rptIdAsString)
-        }
-        assertEquals("PPT_ERRORE_EMESSO_DA_PAA", exception.faultCode)
-    }
-
-    @Test
-    fun `should fallback on fault code on description without fault code`() = runTest {
-        val rptIdAsString = "77777777777302016723749670035"
-        val rptIdAsObject = RptId(rptIdAsString)
-
-        val verificaRPTRIsposta = NodoVerificaRPTRisposta()
-        val esitoVerificaRPT = EsitoNodoVerificaRPTRisposta()
-        esitoVerificaRPT.esito = StOutcome.KO.value()
-        val fault = FaultBean()
-        fault.faultCode = "PPT_ERRORE_EMESSO_DA_PAA"
-        fault.description = ""
-
-        esitoVerificaRPT.fault = fault
-        verificaRPTRIsposta.nodoVerificaRPTRisposta = esitoVerificaRPT
-
-
-        /**
-         * preconditions
-         */
-        given(paymentRequestsInfoRepository.findById(rptIdAsObject)).willReturn(Optional.empty())
-        given(nodoPerPspClient.verificaRpt(any())).willReturn(Mono.just(verificaRPTRIsposta))
-
-        val exception = assertThrows<NodoErrorException> {
-            paymentRequestsService.getPaymentRequestInfo(rptIdAsString)
-        }
-        assertEquals("PPT_ERRORE_EMESSO_DA_PAA", exception.faultCode)
-    }
-
-    @Test
-    fun `should get fault from description if present`() = runTest {
-        val rptIdAsString = "77777777777302016723749670035"
-        val rptIdAsObject = RptId(rptIdAsString)
-
-        val verificaRPTRIsposta = NodoVerificaRPTRisposta()
-        val esitoVerificaRPT = EsitoNodoVerificaRPTRisposta()
-        esitoVerificaRPT.esito = StOutcome.KO.value()
-        val fault = FaultBean()
-        fault.faultCode = "PPT_ERRORE_EMESSO_DA_PAA"
-        fault.description =
-            """
-            FaultString PA: Pagamento in attesa risulta concluso all’Ente Creditore
-            FaultCode PA: PAA_PAGAMENTO_DUPLICATO
-            Description PA:   
-            """.trimIndent()
-        esitoVerificaRPT.fault = fault
-        verificaRPTRIsposta.nodoVerificaRPTRisposta = esitoVerificaRPT
-
-
-        /**
-         * preconditions
-         */
-        given(paymentRequestsInfoRepository.findById(rptIdAsObject)).willReturn(Optional.empty())
-        given(nodoPerPspClient.verificaRpt(any())).willReturn(Mono.just(verificaRPTRIsposta))
-
-        val exception = assertThrows<NodoErrorException> {
-            paymentRequestsService.getPaymentRequestInfo(rptIdAsString)
-        }
-        assertEquals("PAA_PAGAMENTO_DUPLICATO", exception.faultCode)
-    }
-
-    @Test
     fun `should return invalid request for invalid rpt id`() = runTest {
         val rptIdAsString = "invalid rpt id"
         val exception = assertThrows<InvalidRptException> {
@@ -439,13 +185,6 @@ class PaymentRequestsServiceTests {
     fun `should return nodo error from VerifyPaymentNotice KO response with fault bean`() = runTest {
         val rptIdAsString = "77777777777302016723749670035"
         val rptIdAsObject = RptId(rptIdAsString)
-        val verificaRPTRIsposta = NodoVerificaRPTRisposta()
-        val esitoVerificaRPT = EsitoNodoVerificaRPTRisposta()
-        esitoVerificaRPT.esito = StOutcome.KO.value()
-        val fault = FaultBean()
-        fault.faultCode = "PPT_MULTI_BENEFICIARIO"
-        esitoVerificaRPT.fault = fault
-        verificaRPTRIsposta.nodoVerificaRPTRisposta = esitoVerificaRPT
 
         val verifyPaymentNotice = VerifyPaymentNoticeRes()
         verifyPaymentNotice.outcome = StOutcome.KO
@@ -456,7 +195,6 @@ class PaymentRequestsServiceTests {
          * preconditions
          */
         given(paymentRequestsInfoRepository.findById(rptIdAsObject)).willReturn(Optional.empty())
-        given(nodoPerPspClient.verificaRpt(any())).willReturn(Mono.just(verificaRPTRIsposta))
         given(nodeForPspClient.verifyPaymentNotice(any())).willReturn(Mono.just(verifyPaymentNotice))
         /**
          * test
@@ -474,13 +212,6 @@ class PaymentRequestsServiceTests {
     fun `should return nodo error from VerifyPaymentNotice KO response no fault bean`() = runTest {
         val rptIdAsString = "77777777777302016723749670035"
         val rptIdAsObject = RptId(rptIdAsString)
-        val verificaRPTRIsposta = NodoVerificaRPTRisposta()
-        val esitoVerificaRPT = EsitoNodoVerificaRPTRisposta()
-        esitoVerificaRPT.esito = StOutcome.KO.value()
-        val fault = FaultBean()
-        fault.faultCode = "PPT_MULTI_BENEFICIARIO"
-        esitoVerificaRPT.fault = fault
-        verificaRPTRIsposta.nodoVerificaRPTRisposta = esitoVerificaRPT
 
         val verifyPaymentNotice = VerifyPaymentNoticeRes()
         verifyPaymentNotice.outcome = StOutcome.KO
@@ -489,7 +220,6 @@ class PaymentRequestsServiceTests {
          * preconditions
          */
         given(paymentRequestsInfoRepository.findById(rptIdAsObject)).willReturn(Optional.empty())
-        given(nodoPerPspClient.verificaRpt(any())).willReturn(Mono.just(verificaRPTRIsposta))
         given(nodeForPspClient.verifyPaymentNotice(any())).willReturn(Mono.just(verifyPaymentNotice))
         /**
          * test
