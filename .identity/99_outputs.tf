0f4834d6251c5ea4d7f37ada86f4abf672606834
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-06-22 10:27:06 +0200

    add output properties

--- .identity/99_outputs.tf
+++ .identity/99_outputs.tf
@@ -5,3 +5,7 @@ output "tenant_id" {
 output "subscription_id" {
   value = data.azurerm_subscription.current.subscription_id
 }
+
+output "client_id" {
+  value = azuread_service_principal.main.object_id
+}
