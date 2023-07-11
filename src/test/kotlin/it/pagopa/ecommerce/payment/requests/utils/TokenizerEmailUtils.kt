f0c53ca33d88e3b79db19b670abd06d255e9d542
Simone Infante <simone.infante@pagopa.it>
2023-07-11 20:51:54 +0200

    update tests

---
+++ src/test/kotlin/it/pagopa/ecommerce/payment/requests/utils/TokenizerEmailUtils.kt
@@ -0,0 +1,40 @@
+package it.pagopa.ecommerce.payment.requests.utils
+
+import it.pagopa.ecommerce.payment.requests.utils.confidential.ConfidentialDataManager
+import it.pagopa.ecommerce.payment.requests.utils.confidential.domain.Confidential
+import it.pagopa.ecommerce.payment.requests.utils.confidential.domain.Email
+import java.util.*
+import org.junit.Assert.assertEquals
+import org.junit.jupiter.api.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito
+import reactor.core.publisher.Mono
+
+class TokenizerEmailUtilsTests {
+
+  private val confidentialDataManager: ConfidentialDataManager =
+    Mockito.mock(ConfidentialDataManager::class.java)
+
+  private val tokenizerEmailUtils: TokenizerEmailUtils =
+    TokenizerEmailUtils(confidentialDataManager)
+
+  @Test
+  fun shouldEncryptAndDecryptMailSuccessfully() {
+    val email = Email("test@test.com")
+    val emailToken = UUID.randomUUID()
+    val computedConfidential: Confidential<Email> = Confidential(emailToken.toString())
+
+    /* preconditions */ Mockito.`when`(confidentialDataManager.encrypt(email))
+      .thenReturn(Mono.just(computedConfidential))
+    Mockito.`when`(
+        confidentialDataManager.decrypt(
+          ArgumentMatchers.eq(computedConfidential), ArgumentMatchers.any()))
+      .thenReturn(Mono.just(email))
+
+    /* test */
+    val encrypted: Confidential<Email> = tokenizerEmailUtils.toConfidential(email).block()!!
+    val decrypted: Email = tokenizerEmailUtils.toEmail(encrypted).block()!!
+
+    /* assert */ assertEquals(email, decrypted)
+  }
+}
