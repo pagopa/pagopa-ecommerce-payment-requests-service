37bdc41f0d66b94c105174d4e0a53fecdbce2314
Simone Infante <simone.infante@pagopa.it>
2023-07-11 20:30:23 +0200

    fix: format

--- src/test/kotlin/it/pagopa/ecommerce/payment/requests/configurations/confidential/ConfidentialConfigTests.kt
+++ src/test/kotlin/it/pagopa/ecommerce/payment/requests/configurations/confidential/ConfidentialConfigTests.kt
@@ -1,15 +1,12 @@
 package it.pagopa.ecommerce.payment.requests.configurations.confidential
 
-import it.pagopa.ecommerce.payment.requests.configurations.confidential.ConfidentialConfig
-import it.pagopa.ecommerce.payment.requests.configurations.webclients.WebClientsConfig
 import it.pagopa.generated.pdv.v1.api.TokenApi
+import java.util.UUID
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.junit.jupiter.MockitoExtension
-import java.util.UUID
 
 @ExtendWith(MockitoExtension::class)
 class ConfidentialConfigTests {
@@ -17,15 +14,13 @@ class ConfidentialConfigTests {
 
   @Test
   fun `should return valid emailConfidentialDataManager`() = runTest {
-    assertDoesNotThrow {
-      confidentialConfig.emailConfidentialDataManager(TokenApi())
-    }
+    assertDoesNotThrow { confidentialConfig.emailConfidentialDataManager(TokenApi()) }
   }
 
   @Test
   fun `should return valid TokenApi`() = runTest {
     assertDoesNotThrow {
-      confidentialConfig.personalDataVaultApiClient(UUID.randomUUID().toString(),"base-path")
+      confidentialConfig.personalDataVaultApiClient(UUID.randomUUID().toString(), "base-path")
     }
   }
 }
