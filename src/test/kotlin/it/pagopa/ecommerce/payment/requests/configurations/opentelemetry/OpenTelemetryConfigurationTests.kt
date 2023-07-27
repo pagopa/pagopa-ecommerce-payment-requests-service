ade48b515b3e982608e8daf5378bfbce465e5d73
Gianluca Ciuffa <gianlucaciuffa@MacBook-Pro-di-Gianluca.local>
2023-07-27 17:01:50 +0200

    open telemetry configuration test added

---
+++ src/test/kotlin/it/pagopa/ecommerce/payment/requests/configurations/opentelemetry/OpenTelemetryConfigurationTests.kt
@@ -0,0 +1,29 @@
+package it.pagopa.ecommerce.payment.requests.configurations.opentelemetry
+
+import io.opentelemetry.api.GlobalOpenTelemetry
+import io.opentelemetry.api.OpenTelemetry
+import it.pagopa.ecommerce.payment.requests.configurations.openTelemetry.OpenTelemetryConfiguration
+import it.pagopa.ecommerce.payment.requests.configurations.redis.RedisConfiguration
+import it.pagopa.ecommerce.payment.requests.repositories.PaymentRequestInfo
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class OpenTelemetryConfigurationTests {
+    private val openTelemetryConfiguration = OpenTelemetryConfiguration()
+
+    @Test
+    fun `Should get openTelemetry instance`() {
+        val openTelemetry = openTelemetryConfiguration.agentOpenTelemetrySDKInstance()
+        Assertions.assertNotNull(openTelemetry)
+        Assertions.assertTrue(openTelemetry is OpenTelemetry)
+    }
+
+    @Test
+    fun `Should get tracer instance`() {
+        val openTelemetry = openTelemetryConfiguration.agentOpenTelemetrySDKInstance()
+        val tracer = openTelemetryConfiguration.openTelemetryTracer(openTelemetry)
+        Assertions.assertNotNull(tracer)
+        Assertions.assertEquals(tracer, openTelemetry.getTracer("pagopa-ecommerce-payment-requests-service"))
+    }
+
+}
