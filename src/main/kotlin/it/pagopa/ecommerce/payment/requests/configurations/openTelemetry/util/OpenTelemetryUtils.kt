9230c28c2efe25d6514cf482c3b293bd1be1e535
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-07-27 09:48:00 +0200

    replace implementation of opentelemetryutils java in kotlin version

---
+++ src/main/kotlin/it/pagopa/ecommerce/payment/requests/configurations/openTelemetry/util/OpenTelemetryUtils.kt
@@ -0,0 +1,34 @@
+package it.pagopa.ecommerce.payment.requests.configurations.openTelemetry.util
+
+import io.opentelemetry.api.common.Attributes
+import io.opentelemetry.api.trace.StatusCode
+import io.opentelemetry.api.trace.Tracer
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class OpenTelemetryUtils(
+    @Autowired private val openTelemetryTracer: Tracer,
+) {
+
+    fun addSpanWithAttributes(
+        spanName: String?,
+        attributes: Attributes?
+    ) {
+        val span = openTelemetryTracer.spanBuilder(spanName!!).startSpan()
+        span.setAllAttributes(attributes!!)
+        span.end()
+    }
+
+    fun addErrorSpanWithError(
+        spanName: String?,
+        throwable: Throwable?
+    ) {
+        val span = openTelemetryTracer.spanBuilder(spanName!!).startSpan()
+        span
+            .setStatus(StatusCode.ERROR)
+            .recordException(throwable!!)
+        span.end()
+    }
+
+}
