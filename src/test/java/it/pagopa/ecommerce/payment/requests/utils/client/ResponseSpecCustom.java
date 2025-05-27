bcc744a510b30fc3f2b6be1afb49b8c1fee3ba4e
Forciniti Luigi <forciniti@spi.srl>
2025-05-27 16:50:03 +0200

    chore: upgrade Spring Boot from 2.6.7 to 3.4.5

--- src/test/java/it/pagopa/ecommerce/payment/requests/utils/client/ResponseSpecCustom.java
+++ src/test/java/it/pagopa/ecommerce/payment/requests/utils/client/ResponseSpecCustom.java
@@ -1,6 +1,7 @@
 package it.pagopa.ecommerce.payment.requests.utils.client;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -9,14 +10,18 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public abstract class ResponseSpecCustom implements WebClient.ResponseSpec {
-    public abstract HttpStatus getStatus();
 
+    public abstract HttpStatus getStatus(); // Restituisce HttpStatus, compatibile con HttpStatusCode
+
+    @Override
     public WebClient.ResponseSpec onStatus(
-                                           Predicate<HttpStatus> statusPredicate,
+                                           Predicate<HttpStatusCode> statusPredicate,
                                            Function<ClientResponse, Mono<? extends Throwable>> exceptionFunction
     ) {
-        if (statusPredicate.test(this.getStatus()))
-            exceptionFunction.apply(ClientResponse.create(this.getStatus()).build()).block();
+        HttpStatusCode status = getStatus();
+        if (statusPredicate.test(status)) {
+            exceptionFunction.apply(ClientResponse.create(status).build()).block();
+        }
         return this;
     }
 }
