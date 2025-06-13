dbace566b4b78b67975ce966a8d01a43f1c355de
danilo.ciano <danilo.ciano@pagopa.it>
2025-06-13 12:43:01 +0200

    feat(api-key): run spotless-apply

--- src/main/kotlin/it/pagopa/ecommerce/payment/requests/validation/ApiKeyFilter.kt
+++ src/main/kotlin/it/pagopa/ecommerce/payment/requests/validation/ApiKeyFilter.kt
@@ -9,20 +9,20 @@ import reactor.core.publisher.Mono
 
 @Component
 class ApiKeyFilter(
-    @Value("\${security.apiKey.primary}") private val primaryApiKey: String,
-    @Value("\${security.apiKey.secondary}") private val secondaryApiKey: String,
+  @Value("\${security.apiKey.primary}") private val primaryApiKey: String,
+  @Value("\${security.apiKey.secondary}") private val secondaryApiKey: String,
 ) : WebFilter {
-    private val pathsToCheck = listOf("/carts", "/payment-requests")
+  private val pathsToCheck = listOf("/carts", "/payment-requests")
 
-    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
-        val path = exchange.request.path.toString()
-        if (pathsToCheck.any { path.startsWith(it) }) {
-            val apiKey = exchange.request.headers.getFirst("X-Api-Key")
-            if (apiKey.isNullOrBlank() || (apiKey != primaryApiKey && apiKey != secondaryApiKey)) {
-                exchange.response.statusCode = org.springframework.http.HttpStatus.UNAUTHORIZED
-                return exchange.response.setComplete()
-            }
-        }
-        return chain.filter(exchange)
+  override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
+    val path = exchange.request.path.toString()
+    if (pathsToCheck.any { path.startsWith(it) }) {
+      val apiKey = exchange.request.headers.getFirst("X-Api-Key")
+      if (apiKey.isNullOrBlank() || (apiKey != primaryApiKey && apiKey != secondaryApiKey)) {
+        exchange.response.statusCode = org.springframework.http.HttpStatus.UNAUTHORIZED
+        return exchange.response.setComplete()
+      }
     }
+    return chain.filter(exchange)
+  }
 }
