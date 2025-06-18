389f84e897a9ddde1f034daa73816be6bbf776ef
danilo.ciano <danilo.ciano@pagopa.it>
2025-06-18 10:06:16 +0200

    feat(api-key): fix import

--- src/main/kotlin/it/pagopa/ecommerce/payment/requests/validation/ApiKeyFilter.kt
+++ src/main/kotlin/it/pagopa/ecommerce/payment/requests/validation/ApiKeyFilter.kt
@@ -3,6 +3,7 @@ package it.pagopa.ecommerce.payment.requests.validation
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
