31df02e7e7d9e435a5cd5e55fbb5ac775aae5755
danilo.ciano <danilo.ciano@pagopa.it>
2025-08-14 10:30:42 +0200

    feat(graalvm): add redis otel configuration

--- src/main/kotlin/it/pagopa/ecommerce/payment/requests/configurations/openTelemetry/OpenTelemetryConfiguration.kt
+++ src/main/kotlin/it/pagopa/ecommerce/payment/requests/configurations/openTelemetry/OpenTelemetryConfiguration.kt
@@ -1,10 +1,15 @@
 package it.pagopa.ecommerce.payment.requests.configurations.openTelemetry
 
+import io.lettuce.core.resource.ClientResources
+import io.lettuce.core.resource.DefaultClientResources
+import io.lettuce.core.tracing.Tracing
 import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.OpenTelemetry
 import io.opentelemetry.api.trace.Tracer
+import io.opentelemetry.instrumentation.lettuce.v5_1.LettuceTelemetry
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+
 
 @Configuration
 class OpenTelemetryConfiguration {
@@ -17,5 +22,16 @@ class OpenTelemetryConfiguration {
   @Bean
   fun openTelemetryTracer(openTelemetry: OpenTelemetry): Tracer {
     return openTelemetry.getTracer("pagopa-ecommerce-payment-requests-service")
+  }
+
+  //Manual configuration of Redis instrumentation
+  @Bean
+  fun otelLettuceClientResources(openTelemetry: OpenTelemetry): ClientResources {
+    val telemetry: LettuceTelemetry = LettuceTelemetry.builder(openTelemetry).build()
+    val tracing: Tracing = telemetry.newTracing()
+
+    return DefaultClientResources.builder()
+      .tracing(tracing)
+      .build()
   }
 }
