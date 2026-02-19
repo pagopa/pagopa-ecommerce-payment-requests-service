0781b9f8009306129adc1ad088bd46c5b438152f
Forciniti Luigi <forciniti@spi.srl>
2026-02-19 15:38:32 +0100

    chore: fix import error

--- src/main/kotlin/it/pagopa/ecommerce/payment/requests/controllers/v1/CartsController.kt
+++ src/main/kotlin/it/pagopa/ecommerce/payment/requests/controllers/v1/CartsController.kt
@@ -6,6 +6,7 @@ import it.pagopa.ecommerce.generated.payment.requests.server.v1.model.ClientIdDt
 import it.pagopa.ecommerce.payment.requests.services.v1.CartService
 import it.pagopa.ecommerce.payment.requests.warmup.annotations.WarmupFunction
 import it.pagopa.ecommerce.payment.requests.warmup.exceptions.WarmUpException
+import it.pagopa.ecommerce.payment.requests.warmup.utils.v1.WarmupRequests
 import java.net.URI
 import java.time.Duration
 import java.time.temporal.ChronoUnit
